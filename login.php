57fdafae3d2806738a91469d27e1453023a1d7ee
tushar20008 <tusharanand20008@gmail.com>
2018-01-26 15:54:12 +0800

    Create dashboard file

--- login.php
+++ login.php
@@ -22,13 +22,7 @@ $fmsg = "Invalid Login Credentials.";
 }
 //3.1.4 if the user is logged in Greets the user with message
 if (isset($_SESSION['username'])){
-$username = $_SESSION['username'];
-echo "Hai " . $username . "
-";
-echo "This is the Members Area
-";
-echo "<a href='logout.php'>Logout</a>";
- 
+  require('dashboard.php');
 }else{
 //3.2 When the user visits the page first time, simple login form will be displayed.
 ?>
