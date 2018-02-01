47305ff20ba71426d613c86db4ca8df041eb10db
tushar20008 <tusharanand20008@gmail.com>
2018-02-01 16:04:46 +0800

    Add condition to check if user is customer or admin

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,23 @@
-Welcome to the Dashboard
+<?php
+	$username = $_SESSION['username'];
+
+	$query = "SELECT role FROM `User` WHERE username='$username'";
+	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+	$role = mysqli_fetch_array($result)[0];
+?>
+
+<?php
+	if($role == 'customer'){
+?>
+
+	Welcome Noob
+
+<?php
+	}
+	else{
+?>
+
+	Welcome Pro
+<?php
+	}
+?>
